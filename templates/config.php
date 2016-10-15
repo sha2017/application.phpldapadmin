237cf366365e76f54a3012c2e5107aa7d8dce6ba
Matthew Frost <m.frost@Mattronix.nl>
2016-10-15 22:28:38 +0200

    dynamic config

--- templates/config.php
+++ templates/config.php
@@ -10,20 +10,18 @@ $config->custom->appearance['friendly_attrs'] = array(
 $servers = new Datastore();
 
 
+{% for host in groups['wehost_ldap_servers'] %}
 $servers->newServer('ldap_pla');
-$servers->setValue('server','name','ldap01.sha2017.nl');
-$servers->setValue('server','host','ldap01.sha2017.nl');
-$servers->setValue('server','base',array('dc=sha2017,dc=nl'));
-$servers->setValue('login','auth_type','session');
-$servers->setValue('login','bind_id','cn=admin,dc=sha2017,dc=nl');
-
-$servers->newServer('ldap_pla');
-$servers->setValue('server','name','ldap02.sha2017.nl');
-$servers->setValue('server','host','ldap02.sha2017.nl');
+$servers->setValue('server','name','{{ host }}');
+$servers->setValue('server','host','{{ host }}');
 $servers->setValue('server','port',389);
-$servers->setValue('server','base',array('dc=sha2017,dc=nl'));
+$servers->setValue('server','base',array('{{ openldap_.domain_base }}'));
 $servers->setValue('login','auth_type','session');
-$servers->setValue('login','bind_id','cn=admin,dc=sha2017,dc=nl');
-$servers->setValue('server','tls',false);
+$servers->setValue('login','bind_id','{{ openldap_.binduser }}');
+$servers->setValue('server','tls',{{ openldap_.ssl }});
+
+{% endfor %}
+
+
 
 ?>
